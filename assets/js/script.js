af98a9ff91dbfd3367c9539eb2a50077cd400429
manjurulAlamMahi <manjurulalammahi@gmail.com>
2023-01-23 22:52:09 +0600

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,5 +11,175 @@ $(function () {
       document.getElementById('qnt').value ++;
   });
 
+  $('.banner_slider').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+  });
+
+  $('.category_slider').slick({
+    dots: false,
+    arrows: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    nextArrow: '<i class="fas fa-arrow-right cate_next"></i>',
+    prevArrow: '<i class="fas fa-arrow-left cate_prev"></i>',
+    responsive: [
+      {
+        breakpoint: 575.98,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
+
+  $('.rv_silder_carousel').slick({
+    dots: false,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    asNavFor: '.rv_silder_nav'
+  });
+  $('.rv_silder_nav').slick({
+      dots: false,
+      arrows: false,
+      vertical: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      focusOnSelect: true,
+      verticalSwiping: true,
+      asNavFor: '.rv_silder_carousel',
+      responsive: [
+          {
+              breakpoint: 576,
+              settings: {
+                  slidesToShow: 2,
+                  verticalSwiping: false,
+              }
+          },
+      ]
+  });
+
+  $('.clients_slider').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    responsive: [
+      {
+        breakpoint: 575.98,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
+
+  $('.sponsers_row').slick({
+    dots: false,
+    arrows: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: false,
+    responsive: [
+      {
+        breakpoint: 575.98,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
+
+  $('.news_slider').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: false,
+    responsive: [
+      {
+        breakpoint: 575.98,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
+
 
 });
